5fee05ce94cc328a0a0c60d341ad2139b13c2a5a
Francisco Duarte <francisco.caramez.duarte@gmail.com>
2022-02-10 15:15:53 +0000

    lab done

--- index.js
+++ index.js
@@ -1,23 +1,82 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  /* .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
+  }) */
+  /* .then(() => {
+    function addRecipe(
+      title,
+      level,
+      ingredients,
+      cuisine,
+      dishType,
+      image,
+      duration,
+      creator,
+      created
+    ) {
+      const recipe = new Recipe({
+        title: title,
+        level: level,
+        ingredients: ingredients,
+        cuisine: cuisine,
+        dishType: dishType,
+        image: image,
+        duration: duration,
+        creator: creator,
+        created: created,
+      });
+      recipe
+        .save()
+        .then((newRecipe) => console.log(newRecipe.title))
+        .catch((err) => console.log(err));
+    }
+
+    addRecipe(
+      "Pasta",
+      "Easy Peasy",
+      ["pasta", "tomato sauce"],
+      "italian",
+      "main_course",
+      "",
+      12,
+      "Xico"
+    );
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    function addAll(data) {
+      Recipe.insertMany(data)
+        .then((recipes) => recipes.forEach(recipe => {
+          console.log(recipe.title)
+        }))
+        .catch((err) => console.log(err));
+    }
+    addAll(data)
+  }) */
+  /* .then(() => {
+    Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+      .then(() => console.log('Success!'))
+      .catch((err) => console.log(err))
+  }) */
+  .then(() => {
+    Recipe.deleteOne({ title: "Carrot Cake"})
+    .then(() => console.log('Success!'))
+    .catch((err) => console.log(err))
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+
